ac03f9f15e37221959ec0c3fd34ac93124f6083a
Thurston99 <70233944+Thurston99@users.noreply.github.com>
2022-02-17 11:17:11 -0600

    Initialize activity-3

---
+++ blog/src/pages/index.js
@@ -0,0 +1,58 @@
+import * as React from "react"
+import { graphql, Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <Seo title="Home" />
+    <ul>
+    {
+      data.allContentfulBlogPost.edges.map(edge => (
+        <li>
+          <Link to={edge.node.slug} key={edge.node.id}>{edge.node.title}</Link>
+          <div>
+            <GatsbyImage
+              image={edge.node.heroImage.gatsbyImageData}
+              alt="test"
+            />
+          </div>
+          <div>
+            {edge.node.body.childMarkdownRemark.excerpt}
+          </div>
+        </li>
+      ))
+    }
+    </ul>
+  </Layout>
+)
+
+export default IndexPage
+
+export const query = graphql`
+  {
+    allContentfulBlogPost {
+      edges {
+        node {
+          id
+          title
+          slug
+          body {
+            childMarkdownRemark {
+              excerpt
+            }
+          }
+          heroImage {
+            gatsbyImageData(
+              layout: CONSTRAINED
+              placeholder: BLURRED
+              width: 300
+            )
+          }
+        }
+      }
+    }
+  }
+`
