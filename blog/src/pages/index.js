6ef6c6dc0b573485e1f290b9e3d023146bcb2989
Thurston99 <70233944+Thurston99@users.noreply.github.com>
2022-02-17 11:17:11 -0600

    Initialize activity-2

---
+++ blog/src/pages/index.js
@@ -0,0 +1,36 @@
+import * as React from "react"
+import { graphql, Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <Seo title="Home" />
+    <ul>
+    {
+      data.allContentfulBlogPost.edges.map(edge => (
+        <li>
+          <Link to={edge.node.slug} key={edge.node.id}>{edge.node.title}</Link>
+        </li>
+      ))
+    }
+    </ul>
+  </Layout>
+)
+
+export default IndexPage
+
+export const query = graphql`
+  {
+    allContentfulBlogPost {
+      edges {
+        node {
+          id
+          title
+          slug
+        }
+      }
+    }
+  }
+`
