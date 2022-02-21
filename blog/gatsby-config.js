d3b51d0fd54634a06cf837b699d11b4527c6e17d
Thurston Alswager <thurston_alswager@yahoo.com>
2022-02-21 16:39:15 -0600

    Completed Activity4

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-contentful`,
       options: {
